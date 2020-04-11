a39c60cb59925891ac077f51d90f23aa4004d523
unknown <fred.ogunseye@gmail.com>
2020-04-11 01:31:31 -0400

    add inout from file

--- Project/src/proj/Project.java
+++ Project/src/proj/Project.java
@@ -1,17 +1,38 @@
 package proj;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Project {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World");
 		
-		Scanner in = new Scanner(System.in);
-		String s = in.nextLine();
 		
-		System.out.println(s);
+		Scanner in = new Scanner(new File("students.txt"));
+		
+		
+		
+			List<String> students = new ArrayList<String>();
+			while(in.hasNextLine()) {
+				
+				students.add(in.nextLine());
+			}
+			for(int i=0; i < students.size(); i++) {
+				
+				System.out.println("name: " + students.get(i));
+				
+		in.close();
+		
+		/*
+			*/
+			
+		}
+		
 	}
-
 }
+
+
