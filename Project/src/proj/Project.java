e9d062989e5085b502c1f5e59ee9dfcae68c9242
unknown <fred.ogunseye@gmail.com>
2020-04-11 00:36:43 -0400

    initial commit

---
+++ Project/src/proj/Project.java
@@ -0,0 +1,17 @@
+package proj;
+
+import java.util.Scanner;
+
+public class Project {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		
+		Scanner in = new Scanner(System.in);
+		String s = in.nextLine();
+		
+		System.out.println(s);
+	}
+
+}
